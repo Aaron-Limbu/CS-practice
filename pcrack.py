7c8bf334be1718f307bd49ba48d065bd229d3099
Aaron Limbu <aaronlimbu381@gmail.com>
2024-11-12 20:18:24 +0545

    new code

---
+++ pcrack.py
@@ -0,0 +1,18 @@
+import hashlib
+
+
+#simple 
+def crackpass(input_hash,wordlist):
+    with open(wordlist,'r') as file : 
+        for word in file : 
+            word = word.strip()
+            if hashlib.md5(word.encode()).hexdigest() == input_hash:
+                print(f"password found: {word}")
+                return 
+    print("Password not found")
+
+input_hash=input("Enter hash to crack: ")
+wordlist = "wordlists.txt"
+crackpass(input_hash,wordlist)
+
+#update on code
